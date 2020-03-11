df89ad90a2f09fc3d8221a9904eae33b1d559b6b
Yohan Ryoo <yohanryoo1@gmail.com>
2020-03-11 13:06:53 +1300

    update text for about page

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title: 'About me',
+        title: 'About',
         name: 'Yohan Ryoo'
     })
 })
